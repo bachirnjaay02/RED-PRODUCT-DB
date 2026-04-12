0c64f1e4db6bc64c30490e748b364a56bdc2d09a
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-12 16:08:08 +0000

    fix: remove duplicate CORS origin

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://red-product-front-mzvk.vercel.app, https://red-product-db-production.up.railway.app'],
+    'allowed_origins' => ['https://red-product-front-mzvk.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
