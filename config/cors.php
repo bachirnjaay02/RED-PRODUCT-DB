dec849a51d14fbed884569b21d1793dcd85a0b74
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-13 11:39:44 +0000

    add allowed origins patterns

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://red-product-front-mzvk.vercel.app'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['#^https://red-product-front-mzvk.*\.vercel\.app$#'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
