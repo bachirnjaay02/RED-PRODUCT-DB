5701d599b0a7a516c766cfb178b9312e2ac86721
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-15 15:13:35 +0000

    fix: debug build

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,25 +5,15 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-
-// Ajouter le middleware CORS pour gérer les requêtes cross-origin
-
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->use([
-            \Illuminate\Http\Middleware\HandleCors::class,  
-        ]);
-        
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    
-    
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        
+        //
     })->create();
-
-
