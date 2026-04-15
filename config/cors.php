f3c827c3725d33e82837d3b3f9dfa67720bd197c
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-15 14:49:41 +0000

    fix: review cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,12 +3,13 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://red-product-front-mzvk.vercel.app'],
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins' => [
+        'https://red-product-front-mzvk.vercel.app',
+        'https://red-product-front-mzvk-dh1z0emlw-bachirnjaay02s-projects.vercel.app',
+    ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => false,
-]; 
-
-// #^https://red-product-front-mzvk.*\.vercel\.app$#
+];
