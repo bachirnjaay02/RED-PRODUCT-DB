8d18afaca5f951b99db1a575ca8903570394b481
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-15 15:30:21 +0000

    fix: actif par defeaut

--- database/migrations/2026_04_14_175417_add_activation_to_users_table.php
+++ database/migrations/2026_04_14_175417_add_activation_to_users_table.php
@@ -9,10 +9,12 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    // je veux les utilisateurs soient actif par defeaut meme si ils n'ont pas encore activer leur compte via le lien d'activation, pour eviter les problemes d'inscription
+
    public function up() {
     Schema::table('users', function (Blueprint $table) {
-        $table->boolean('is_active')->default(false); // Faux par défaut
-        $table->string('activation_token')->nullable();
+        $table->boolean('is_active')->default(true); // ✅ Par défaut, les utilisateurs ne sont pas actifs jusqu'à ce qu'ils activent leur compte via le lien d'activation
+        $table->string('activation_token')->nullable();  // ✅ On rend le token nullable pour éviter les problèmes d'inscription
     });
 }
 
