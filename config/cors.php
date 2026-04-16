553d0cc1fcb10480f30659a2ef8a00e5db76957d
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-16 16:41:38 +0000

    fix: corrige cors.php

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,6 @@ return [
     'allowed_origins_patterns' => ['#^https://red-product-.*\.vercel\.app$#'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 86400,
-    'supports_credentials' => false,
+    'max_age' => 0,
+    'supports_credentials' => true,
 ];
