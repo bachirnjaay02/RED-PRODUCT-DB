6719b0fe861cb046bc8a57fbe4f9cec840e98786
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-16 16:38:39 +0000

    fix: delete /

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'https://red-product-front-1.vercel.app/',
+        'https://red-product-front-1.vercel.app',
     ],
     'allowed_origins_patterns' => ['#^https://red-product-.*\.vercel\.app$#'],
     'allowed_headers' => ['*'],
