cc3817fc4b24f7b6d958095601ba841eb38a5717
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-20 14:32:56 +0000

    add route web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+ 
+Route::get('/manifest.json', function () {
+    return response()->json([
+        'name' => 'Red Product',
+        'short_name' => 'RedProduct',
+        'start_url' => '/',
+        'display' => 'standalone',
+        'background_color' => '#ffffff',
+        'theme_color' => '#000000',
+        'icons' => []
+    ]);
+})->withoutMiddleware(['auth']);
