aae95e2477b12f352ce15b26f8c2985a7b80f02d
BASSIROUNDIAYE233 <bachirndiaye233@gmail.com>
2026-04-21 10:59:14 +0000

    fix:...

--- config/cors.php
+++ config/cors.php
@@ -3,9 +3,8 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://red-product-front-1.vercel.app',
-        'https://red-product-front-1-f4dz8ohpo-bachirnjaay02s-projects.vercel.app', // ← Ajoutez cette URL
+    'allowed_origins' => [ 'https://red-product-front-1.vercel.app',
+        'https://red-product-front-1-f4dz8ohpo-bachirnjaay02s-projects.vercel.app', // Ajoutez cette URL
     ],
     'allowed_origins_patterns' => ['#^https://red-product-.*\.vercel\.app$#'],
     'allowed_headers' => ['*'],
